087e17ef912c55bdbbe004c830f73146de5f4819
Jeff Liu <jeff.liu@jxto.com>
2017-11-13 17:18:09 +0800

    more template

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var ejs = require('ejs');
 var fs = require('fs');
 
-function gen(data, templateFile, outputFile){
+function gen(data, templateFile, outputFile, option){
     var htmlContent = fs.readFileSync(__dirname + templateFile, 'utf8');
-    var output = ejs.render(htmlContent, data);
+    var output = ejs.render(htmlContent, data, option);
     fs.writeFile(outputFile, output, function(err) {
         console.log('Generating: ', templateFile, ",", outputFile);
         if(err) {
@@ -13,10 +13,18 @@ function gen(data, templateFile, outputFile){
     }); 
 }
 
-var data = require('./data/data.json');
-gen(data,'/tmpl/form.ejs', "./output/form");
-gen(data,'/tmpl/table.ejs',"./output/table" );
+var strs = ["user","product"];
 
-var user = require('./data/user.json');
-gen(user,'/tmpl/form.ejs', "./output/user/form");
-gen(user,'/tmpl/table.ejs',"./output/user/table" )
+strs.forEach(function(s){
+    var data = require('./data/'+s+'.json');
+    gen(data,'/tmpl/form.ejs', "./output/"+s+"/form");
+    gen(data,'/tmpl/table.ejs',"./output/"+s+"/table");
+    gen(data,'/tmpl/view/detail-js.ejs', "./output/"+s+"/form-js", {delimiter: '?'});
+    gen(data,'/tmpl/view/list-js.ejs', "./output/"+s+"/list-js", {delimiter: '?'});
+});
+
+var routers = ["product"];
+routers.forEach(function(s){
+    var data = require('./data/'+s+'-router.json');
+    gen(data,'/tmpl/router.ejs', "./output/route/"+s);
+});
